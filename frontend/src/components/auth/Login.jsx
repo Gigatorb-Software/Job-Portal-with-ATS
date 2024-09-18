60fc512fbca7594474a00efc11c7584269d1fb2f
amit.yadav@gigatorb.com <amit.yadav@gigatorb.com>
2024-09-18 18:47:44 +0530

    ui changes

--- frontend/src/components/auth/Login.jsx
+++ frontend/src/components/auth/Login.jsx
@@ -102,7 +102,7 @@ const Login = () => {
               value={input.email}
               name="email"
               onChange={changeEventHandler}
-              placeholder="please enter your email"
+              placeholder="Enter your email"
             />
           </div>
           <div className="my-2">
@@ -112,7 +112,7 @@ const Login = () => {
               value={input.password}
               name="password"
               onChange={changeEventHandler}
-              placeholder="please enter your password"
+              placeholder="Enter your password"
             />
           </div>
           <div className="flex items-center justify-between">
